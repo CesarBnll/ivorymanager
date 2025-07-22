12c5644912644b6f8e6cd9678bcf4ddda86a0bac
Cesar Bonilla <65072957+CesarBnll@users.noreply.github.com>
2025-07-22 13:13:08 -0400

    Update database.js
    Config vars added

--- database/database.js
+++ database/database.js
@@ -2,14 +2,14 @@ const { createConnection } = require('promise-mysql');
 //const { createConnection } = require('mysql2/promise.js');
 
 const connection = createConnection({
-    host: process.env.HOST,
-    port: process.env.PORT,
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
-console.log("DOTENV PORT: " + process.env.PORT);
+console.log("DOTENV PORT: " + process.env.DB_PORT);
 
 const getConnection = () => {
     return connection;
