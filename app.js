b4bb08923123b3152fec7392e5b0eb5c1d7634d0
Cesar Bonilla <65072957+CesarBnll@users.noreply.github.com>
2025-07-22 13:02:17 -0400

    Test

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var ejs = require('ejs');
 
+console.log("PATH HERE: " + path)
+
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
